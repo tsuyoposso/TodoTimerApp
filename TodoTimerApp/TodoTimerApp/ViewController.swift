a1e3722cdf0375364d3a7a69742394d37f906fd1
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-12 19:16:21 +0900

    first commit

---
+++ TodoTimerApp/TodoTimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoTimerApp
+//
+//  Created by 長坂豪士 on 2019/10/12.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
